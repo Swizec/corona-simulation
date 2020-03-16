708745460d2319da12192231e50017a2a34745ee
Swizec Teller <swizec@swizec.com>
2020-03-16 07:17:06 -0700

    Make stuff configurable via params

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import "./App.css";
 import { Population } from "./Population";
 
 function App() {
+    const query = new URLSearchParams(window.location.search);
+
     return (
         <div className="App">
-            <h1>Visualizing the spread of viruses in a population</h1>
-            <Population cx={400} cy={200} width={400} height={300} />
+            <h1>
+                {query.get("title") ||
+                    "Visualizing the spread of viruses in a population"}
+            </h1>
+            <Population
+                cx={400}
+                cy={200}
+                width={400}
+                height={300}
+                defaultMortality={query.get("mortality") || 4}
+                defaultVirality={query.get("virality") || 50}
+                defaultLengthOfInfection={query.get("lengthOfInfection") || 40}
+                defaultSocialDistancing={query.get("socialDistancing") || 0}
+                defaultReinfectability={query.get("reinfectability") || 30}
+            />
         </div>
     );
 }
